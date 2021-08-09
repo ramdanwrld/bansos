e9a6ecc55931420f8b7fa03b7fe1503f8ebb12d0
Nuraena Ramdani <ramdanwrld@gmail.com>
2021-08-09 15:24:07 +0700

    update iui
    ramdan update ui

---
+++ resources/views/index.blade.php
@@ -0,0 +1,379 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>SOLIDARITAS - Sistem Online Data Penerima Bantuan Sosial Kabupaten Ciamis</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+  
+  <!-- Favicons -->
+  <link href="assets/img/favicon.png" rel="icon">
+  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Dosis:300,400,500,,600,700,700i|Lato:300,300i,400,400i,700,700i" rel="stylesheet">
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.2/css/all.css" />
+  <!-- Vendor CSS Files -->
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+  <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+  <!-- MDB -->
+  <link href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/3.6.0/mdb.min.css"rel="stylesheet">
+  <!-- MDB -->
+  <link rel="stylesheet" href="css/mdb.min.css" />
+
+  <!-- Template Main CSS File -->
+  <link href="assets/css/style.css" rel="stylesheet">
+
+  <!-- =======================================================
+  * Template Name: Butterfly - v4.3.0
+  * Template URL: https://bootstrapmade.com/butterfly-free-bootstrap-theme/
+  * Author: BootstrapMade.com
+  * License: https://bootstrapmade.com/license/
+  ======================================================== -->
+    <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+</head>
+
+<body>
+
+  <!-- ======= Header ======= -->
+  <header id="header" class="fixed-top">
+    <div class="container d-flex align-items-center justify-content-between">
+
+      <a href="index.html" class="logo"><img src="assets/img/logo_solidaritas.png" alt="" class="img-fluid"></a>
+      <!-- Uncomment below if you prefer to use text as a logo -->
+      <!-- <h1 class="logo"><a href="index.html">Butterfly</a></h1> -->
+
+      <nav id="navbar" class="navbar">
+        <ul>
+          <li><a class="nav-link scrollto active" href="#hero">BERANDA</a></li>
+          <li><a class="nav-link scrollto" href="#hotline">PENGADUAN</a></li>
+          <li><a class="nav-link scrollto" href="#">PENCARIAN</a></li>
+      </nav><!-- .navbar -->
+
+    </div>
+  </header><!-- End Header -->
+
+  <!-- ======= Hero Section ======= -->
+  <section id="hero" class="d-flex align-items-center">
+
+  <div class="container">
+	<div class="row">
+		<div class="col-lg-6 d-flex flex-column justify-content-center pt-4 pt-lg-0 order-2 order-lg-1" data-aos="fade-up" data-aos-delay="200">
+		      <h2>Selamat datang di portal</h2>
+          <h1>Sistem Online Data Penerima</h1>
+          <h1> Bantuan Sosial</h1>
+          <h2>Kabupaten Ciamis</h2>
+          <p>Program bantuan sosial dirancang pemerintah untuk mendukung kesejahteraan masyarakat yang terdampak ekonominya akibat pandemi COVID-19.
+            <a href="assets/img/bansos-1.png"class="glightbox play-btn mb-4">       
+            <u><strong>Lihat Selengkapnya..</strong></u></a><p>      
+          <p>Sekarang Anda dapat melihat daftar penerima bantuan di wilayah Anda.<p>
+          <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
+            Lihat Daftar Penerimaan Bantuan
+          </button>
+          <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+          <div class="modal-dialog" role="document">
+            <div class="modal-content">
+              <div class="modal-header">
+                  <h5 class="modal-title" id="exampleModalLabel">PENCARIAN INFORMASI LEBIH LENGKAP</h5>
+                  <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                  </button>
+                </div>
+                <div class="modal-body">  
+                  <form>
+                    <div class="container">
+                      <div class="row">
+                        <div class="col-sm">
+                          <div class="modal-body">
+                            <div class="dropdown show">
+                              <a class="btn btn-secondary dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                Dropdown link
+                              </a>                           
+                              <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+                                <a class="dropdown-item" href="#">Action</a>
+                                <a class="dropdown-item" href="#">Another action</a>
+                                <a class="dropdown-item" href="#">Something else here</a>
+                              </div>
+                            </div>
+                            <div class="dropdown show">
+                              <a class="btn btn-secondary dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                Dropdown link
+                              </a>                            
+                              <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+                                <a class="dropdown-item" href="#">Action</a>
+                                <a class="dropdown-item" href="#">Another action</a>
+                                <a class="dropdown-item" href="#">Something else here</a>
+                              </div>
+                            </div>
+                            <div class="dropdown show">
+                              <a class="btn btn-secondary dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                Dropdown link
+                              </a>
+                            
+                              <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+                                <a class="dropdown-item" href="#">Action</a>
+                                <a class="dropdown-item" href="#">Another action</a>
+                                <a class="dropdown-item" href="#">Something else here</a>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      <div class="col-sm">
+                        <div class="modal-body">
+                          <div class="form-group">
+                            <label for="email1">Email address</label>
+                            <div class="dropdown show">
+                              <a class="btn btn-secondary dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                Dropdown link
+                              </a>
+                            
+                              <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+                                <a class="dropdown-item" href="#">Action</a>
+                                <a class="dropdown-item" href="#">Another action</a>
+                                <a class="dropdown-item" href="#">Something else here</a>
+                              </div>
+                          </div>
+                          <div class="form-group">
+                            <label for="password1">Password</label>
+                            <input type="password" class="form-control" id="password1" placeholder="Password">
+                          </div>
+                          <div class="form-group">
+                            <label for="password1">Confirm Password</label>
+                            <input type="password" class="form-control" id="password2" placeholder="Confirm Password">
+                          </div>
+                        </div>
+                        <div class="modal-footer border-top-0 d-flex justify-content-center">
+                          <button type="submit" class="btn btn-success">Submit</button>
+                        </div>
+                      </div>
+                    </div>
+                    </div> 
+                  </form>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+		</div>
+        <div class="col-lg-6 order-1 order-lg-2 hero-img">
+          <img src="assets/img/hero-img.png" class="img-fluid" alt="">
+        </div>
+      </div>
+    </div>
+
+  </section><!-- End Hero -->
+    <!-- ======= Services Section ======= -->
+    <section id="services" class="services section-bg">">
+        <div class="container">
+          <div class="row">
+            <div class="col-lg-4 col-md-6">
+              <div class="icon-box">
+                <h4 class="title"><a href="">Pencarian Penerima Bantuan</a></h4>
+                <p class="description">Anda dapat melakukan pencarian penerima bantuan dengan cara input nomor NIK di bawah ini.</p>
+                <div class="input-group">
+                  <div class="form-outline">
+                    <input type="search" id="form1" class="form-control" />
+                    <label class="form-label" for="form1">Search</label>
+                  </div>
+                  <button type="button" class="btn btn-primary">
+                    <i class="fas fa-search"></i>
+                  </button>
+                </div>
+              </div>
+            </div>
+            <div class="col-lg-4 col-md-6">
+              <div class="icon-box">
+                <h4 class="title"><a href="">Informasi Penolakan</a></h4>
+                <p class="description">Untuk mengetahui kenapa anda tidak lolos dalam penetapan bansos provinsi Anda dapat mengisi NIK di bawah ini. Untuk keterangan lebih lanjut seputar Informasi Penolakan Klik Disini</p>
+                <div class="input-group">
+                  <div class="form-outline">
+                    <input type="search" id="form1" class="form-control" />
+                    <label class="form-label" for="form1">Search</label>
+                  </div>
+                  <button type="button" class="btn btn-primary">
+                    <i class="fas fa-search"></i>
+                  </button>
+                </div>
+              </div>
+            </div>
+  
+            <div class="col-lg-4 col-md-6" data-wow-delay="0.1s">
+              <div class="icon-box">
+                <h4 class="title"><a href="">Lacak Penyaluran</a></h4>
+                <p class="description">Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur</p>
+                <div class="input-group">
+                  <div class="form-outline">
+                    <input type="search" id="form1" class="form-control" />
+                    <label class="form-label" for="form1">Search</label>
+                  </div>
+                  <button type="button" class="btn btn-primary">
+                    <i class="fas fa-search"></i>
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+    </section><!-- End Services Section -->
+  <main id="main">
+
+    <!-- ======= Services Section ======= -->
+    <section id="services" class="services section-bg">
+      <div class="container">
+
+        <div class="section-title">
+          <h2>STATISTIK BANTUAN SOSIAL</h2>
+        </div>
+
+        <div class="row">
+          <div class="col-sm">
+            <h4 class="title"><a href="">Kecamatan</a></h4>
+              <p class="description">Lihat statistik jumlah penerima manfaat bantuan sosial pemerintah di seluruh kecamatan di Kabupaten Ciamis.</p>
+              <p>Lihat Selengkapnya</p>
+          </div>
+          <div class="col-sm">
+            <h4 class="title"><a href="">Kelurahan / Desa</a></h4>
+              <p class="description">Lihat statistik jumlah penerima manfaat bantuan sosial pemerintah di seluruh wilayah Provinsi Jawa Barat.</p>
+              <p>Lihat Selengkapnya</p>
+          </div>
+        </div>
+      </div>
+    </section><!-- End Services Section -->
+  <main id="main">
+
+
+    </section><!-- kurir  Section -->
+    <section id="ramdan" class="d-flex align-items-center">
+    <div class="container">
+      <div class="row">
+        <div class="col hero-img">
+          <img src="assets/img/kurir.png" class="img-fluid" alt="">
+        </div>
+        <div class="col-lg-6 d-flex flex-column justify-content-center pt-4 pt-lg-0 order-2 order-lg-1" data-aos="fade-up" data-aos-delay="200">
+          <h1>Alur Pemberian Bantuan Sosial</h1>
+          <h2>Berbasis DTKS dan Non-DTKS</h2>
+          <p>Bagaimana alur pemberian bantuan sosial berbasis DTKS dan Non-DTKS dapat sampai ke masayrakat? Anda dapat melihat alur lengkap dari proses tersebut dengan mengklik tautan yang tersedia di bawah ini.
+          <br></br>
+          <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
+            Lihat Alur Pemberian Bantuan Sosial
+          </button> 
+        </div>
+      </div>
+    </div>
+    </section><!-- End Counts Section -->
+    <section id="ramdan" class="d-flex align-items-center">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-6 pt-4 pt-lg-0 order-2 order-lg-1 d-flex flex-column justify-content-center">
+            <h1>Komponen Bantuan Sosial Tahap III </h1>
+            <p>Ada perubahan komponen bantuan di penyaluran tahap III. Apa saja? </p>
+            <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
+              Cek disini!
+            </button>
+          </div>
+          <div class="col-lg-6 order-1 order-lg-2 hero-img">
+            <img src="assets/img/bansos.png" class="img-fluid" alt="">
+          </div>
+        </div>
+      </div>
+  </section><!-- End Counts Section -->
+  <section id="ramdan" class="d-flex align-items-center">
+  <div class="container">
+    <div class="row">
+      <div class="col hero-img">
+        <img src="assets/img/kurir.png" class="img-fluid" alt="">
+      </div>
+      <div class="col-lg-6 d-flex flex-column justify-content-center pt-4 pt-lg-0 order-2 order-lg-1" data-aos="fade-up" data-aos-delay="200">
+        <h1>Alasan Ditolaknya Pendaftar</h1>
+        <h2>Sebagai Penerima Bantuan Sosial Provinsi</h2>
+        <p>Pada aplikasi Sapawarga dan website Solidaritas, tercantum sekitar 27 alasan detail yang menjelaskan ditolaknya pendaftar sebagai penerima atau Keluarga Rumah Tangga Sasaran (KRTS) Bansos Provinsi.</p>
+        <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
+          Lihat Penjelasan Detail Alasan Ditolaknya Pendaftar
+        </button>
+        </div> 
+      </div>
+    </div>
+  </section><!-- End Counts Section -->
+  <section id="hotline" class="d-flex align-items-center">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-6 pt-4 pt-lg-0 order-2 order-lg-1 d-flex flex-column justify-content-center">
+          <h1>Kontak Hotline Bantuan Sosial</h1>
+          <p>Anda dapat menghubungi Hotline Bantuan Sosial untuk bertanya atau sekedar mencari informasi seputar Bantuan Sosial di Jawa Barat.</p>
+          <button type="button" class="btn btn-primary"> Hubungi hotline kami</button>
+        </div>
+        <div class="col-lg-6 order-1 order-lg-2 hero-img">
+          <img src="assets/img/komputer.png" class="img-fluid" alt="">
+        </div>
+      </div>
+    </div>
+  </div>
+  </section>
+  <section id="services" class="services section-bg">
+    <div class="container">
+
+      <div class="section-title">
+        <h2>Bekerja sama dengan </h2>
+      </div>
+      <div class="row align-items-center">
+        <div class="col-3">
+          <a href="/">
+            <img class="img-responsive" src="assets/img/Lambang_Kabupaten_Ciamis.png">
+          </a>
+        <div class="col-3">
+          <a href="/">
+            <img class="img-responsive" src="assets/img/Lambang_Kabupaten_Ciamis.png">
+          </a>
+        </div>
+        <div class="col-3">
+          <a href="/">
+            <img class="img-responsive" src="assets/img/Lambang_Kabupaten_Ciamis.png">
+          </a>
+        </div>
+        <div class="col-3">
+          <a href="/">
+            <img class="img-responsive" src="assets/img/Lambang_Kabupaten_Ciamis.png">
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>
+  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+  <!-- Vendor JS Files -->
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+  <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+  <script src="assets/vendor/php-email-form/validate.js"></script>
+  <script src="assets/vendor/purecounter/purecounter.js"></script>
+  <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="assets/js/main.js"></script>
+  <!-- Optional JavaScript -->
+  <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+  <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+  <!-- MDB -->
+  <script type="text/javascript" src="js/mdb.min.js"></script>
+  <!-- Custom scripts -->
+  <script type="text/javascript">
+  const searchButton = document.getElementById('search-button');
+  const searchInput = document.getElementById('search-input');
+  searchButton.addEventListener('click', () => {
+  const inputValue = searchInput.value;
+  alert(inputValue);
+  });</script>
+</body>
+
+</html>
---
+++ resources/views/test.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=<div class="modal" tabindex="-1" role="dialog">
+        <div class="modal-dialog" role="document">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title">Modal title</h5>
+              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            <div class="modal-body">
+              <p>Modal body text goes here.</p>
+            </div>
+            <div class="modal-footer">
+              <button type="button" class="btn btn-primary">Save changes</button>
+              <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+            </div>
+          </div>
+        </div>
+      </div>, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
