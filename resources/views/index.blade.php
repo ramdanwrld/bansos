3450daf261fdb6ed01fbd2ac83e9d4bf8ffe86af
Nuraena Ramdani <ramdanwrld@gmail.com>
2021-08-12 09:38:55 +0700

    update logo
    logo pikocis

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -36,7 +36,7 @@
   <!-- ======= Header ======= -->
   <header id="header" class="fixed-top">
     <div class="container d-flex align-items-center justify-content-between">
-      <a href="index.blade.php" class="logo"><img src="assets/img/pikocis.png" alt="" class="img-fluid"></a>
+      <a href="/" class="logo"><img src="assets/img/pikocis.png" alt="" class="img-fluid"></a>
       <!-- Uncomment below if you prefer to use text as a logo -->
       <!-- <h1 class="logo"><a href="index.html">Butterfly</a></h1> -->
 
